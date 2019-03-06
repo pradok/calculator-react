0ef9b4bf3c23fcb5a5dd7db5322a173204fb06b3
Pradosh Sinha <pradoshks@outlook.com>
2019-03-07 00:31:51 +1100

    Add all keys config

---
+++ src/calculator/components/config/keys.ts
@@ -0,0 +1,91 @@
+export const C = {
+    label: "C",
+    value: "clear",
+    type: "function"
+};
+export const zero = {
+    label: "0",
+    value: "0",
+    type: "digit"
+};
+export const one = {
+    label: "1",
+    value: "1",
+    type: "digit"
+};
+export const two = {
+    label: "2",
+    value: "2",
+    type: "digit"
+};
+export const three = {
+    label: "3",
+    value: "3",
+    type: "digit"
+};
+export const four = {
+    label: "4",
+    value: "4",
+    type: "digit"
+};
+export const five = {
+    label: "5",
+    value: "5",
+    type: "digit"
+};
+export const six = {
+    label: "6",
+    value: "6",
+    type: "digit"
+};
+export const seven = {
+    label: "7",
+    value: "7",
+    type: "digit"
+};
+export const eight = {
+    label: "8",
+    value: "8",
+    type: "digit"
+};
+export const nine = {
+    label: "9",
+    value: "9",
+    type: "digit"
+};
+export const divide = {
+    label: "/",
+    value: "/",
+    type: "operator"
+};
+export const multiply = {
+    label: "x",
+    value: "*",
+    type: "operator"
+};
+export const add = {
+    label: "+",
+    value: "+",
+    type: "operator"
+};
+export const subtract = {
+    label: "-",
+    value: "-",
+    type: "operator"
+};
+export const equal = {
+    label: "=",
+    value: "=",
+    type: "operator"
+};
+export const decimal = {
+    label: ".",
+    value: ".",
+    type: "decimal"
+};
+export const none = {
+    label: "",
+    value: "null",
+    type: "null"
+};
+
---
+++ src/calculator/components/config/simpleLayout.ts
@@ -0,0 +1,22 @@
+import {
+    C,
+    zero,
+    one,
+    two,
+    three,
+    four,
+    five,
+    six,
+    seven,
+    eight,
+    nine,
+    divide,
+    multiply,
+    add,
+    subtract,
+    equal,
+    decimal,
+    none
+} from './keys';
+
+export const simpleLayoutItems = [C, seven, four, one, zero, none, eight, five, two, decimal, none, nine, six, three, none, divide, multiply, subtract, add, equal];
